7706b2bdc70b82377477fe512d8cc9de30bfe060
Cavidan Shushayev <csusayev19@gmail.com>
2023-04-10 21:31:13 +0400

    last commit number 2

--- 10aprelhometask/task1/script.js
+++ 10aprelhometask/task1/script.js
@@ -1,4 +1,27 @@
-var eded =20;
+// without function
+var eded=20;
+ if (eded %7 ==0){
+    console.log("7 ye bolunur");
+} else{
+    var qaliq=eded%7;
+    if(qaliq<4){
+     var new_eded=eded-qaliq;
+    console.log(new_eded);}
+    else{
+     var new_eded_2=eded -qaliq+7;
+     console.log(new_eded_2);
+
+    }
+}
+ 
+// with regular function
+var eded2 =20;
+myFunction(eded2);
+
+
+function myFunction(eded) {
+
+
 if (eded %7 ==0){
     console.log("7 ye bolunur");
 } else{
@@ -12,3 +35,31 @@ if (eded %7 ==0){
 
     }
 }
+
+  return new_eded_2;
+}
+
+
+// with anonymous function
+var eded3=20;
+var myfunction2 = function () {
+ 
+
+if (eded %7 ==0){
+    console.log("7 ye bolunur");
+} else{
+    var qaliq=eded%7;
+    if(qaliq<4){
+     var new_eded=eded-qaliq;
+    console.log(new_eded);}
+    else{
+     var new_eded_2=eded -qaliq+7;
+     console.log(new_eded_2);
+
+    }
+}
+
+  return new_eded_2;}
+myfunction2(eded3);
+
+
--- 10aprelhometask/task2/script.js
+++ 10aprelhometask/task2/script.js
@@ -1,28 +1,104 @@
-let eded = 120;
-var say = 0;
+var eded = 120;
+
+
+
+var say2 = 0;
+// without function
+
 if (eded < 50) {
+    
     for (let i = 0; i < eded; i++) {
         if (i != 0 && i % 3 == 0) {
-            say++;
+            say2++;
         }
     }
-    console.log("bu edede qeder 3 e bolunen ededlerin sayi:" + say);
+    console.log("bu edede qeder 3 e bolunen ededlerin sayi:" + say2);
 }
-if (eded > 50 && eded<100) {
+if (eded > 50 && eded < 100) {
     for (let i = 0; i < eded; i++) {
         if (i != 0 && i % 3 == 0) {
-            say++;
+            say2++;
         }
     }
 
-    console.log("bu edede qeder 5 e bolunen ededlerin sayi:" + say);
+    console.log("bu edede qeder 5 e bolunen ededlerin sayi:" + say2);
 }
 
 if (eded > 100) {
     for (let i = 0; i < eded; i++) {
         if (i != 0 && i % 8 == 0) {
-           say++;
+            say2++;
         }
     }
-    console.log("bu edede qeder 8 e bolunen ededlerin sayi:" +say);
+    console.log("bu edede qeder 8 e bolunen ededlerin sayi:" + say2);
+}
+
+
+
+
+// with regular function
+myFunction(eded);
+function myFunction(eded) {
+    var say = 0;
+
+    if (eded < 50) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 3 == 0) {
+                say++;
+            }
+        }
+        console.log("bu edede qeder 3 e bolunen ededlerin sayi:" + say);
+    }
+    if (eded > 50 && eded < 100) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 3 == 0) {
+                say++;
+            }
+        }
+
+        console.log("bu edede qeder 5 e bolunen ededlerin sayi:" + say);
+    }
+
+    if (eded > 100) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 8 == 0) {
+                say++;
+            }
+        }
+        console.log("bu edede qeder 8 e bolunen ededlerin sayi:" + say);
+    }
+}
+
+
+// with annonymus function
+myFunction2(eded);
+function myFunction2(){
+    var say = 0;
+
+    if (eded < 50) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 3 == 0) {
+                say++;
+            }
+        }
+        console.log("bu edede qeder 3 e bolunen ededlerin sayi:" + say);
+    }
+    if (eded > 50 && eded < 100) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 3 == 0) {
+                say++;
+            }
+        }
+
+        console.log("bu edede qeder 5 e bolunen ededlerin sayi:" + say);
+    }
+
+    if (eded > 100) {
+        for (let i = 0; i < eded; i++) {
+            if (i != 0 && i % 8 == 0) {
+                say++;
+            }
+        }
+        console.log("bu edede qeder 8 e bolunen ededlerin sayi:" + say);
+    }
 }
